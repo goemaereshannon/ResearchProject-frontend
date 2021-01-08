4e52194abb391eb26c4202bb344ea4b2956aeb49
Shannon Goemaere <shannon.goemaere@student.howest.be>
2021-01-09 00:06:41 +0100

    Head import + SASS installed

--- e-commerce-app/pages/profile/profile.js
+++ e-commerce-app/pages/profile/profile.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Profile() {
   return (
     <>
+      <Head>
+        <title>My account - ByViChi</title>
+      </Head>
       <h1>PROFILE</h1>
     </>
   );
