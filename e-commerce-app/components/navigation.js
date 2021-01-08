896605cca05f14e46e294d87f0128517435542e8
Shannon Goemaere <shannon.goemaere@student.howest.be>
2021-01-08 23:57:56 +0100

    Added public assets (svgs)

--- e-commerce-app/components/navigation.js
+++ e-commerce-app/components/navigation.js
@@ -5,33 +5,43 @@ export default function Navigation() {
     <>
       <h1>Navigation</h1>
       <nav>
-          <ul>
-              <li>
-                  <Link href="/">
-                      Discover
-                  </Link>
-              </li>
-              <li>
-                  <Link href="/products/search">
-                      <a>Search</a>
-                  </Link>
-              </li>
-              <li>
-                  <Link href="/orders/cart">
-                      <a>Cart</a>
-                  </Link>
-              </li>
-              <li>
-                  <Link href="/profile/favorites">
-                      <a>Favorites</a>
-                  </Link>
-              </li>
-              <li>
-                  <Link href="/profile/profile">
-                      <a>Profile</a>
-                  </Link>
-              </li>
-          </ul>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>
+                <img src="/svgs/home.svg" alt="Discover icon" className="navigationIcon" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/products/search">
+              <a>
+                <img src="/svgs/search.svg" alt="Search icon" className="navigationIcon" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/orders/cart">
+              <a>
+                <img src="/svgs/shopping-bag.svg" alt="Cart icon" className="navigationIcon" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/profile/favorites">
+              <a>
+                <img src="/svgs/like.svg" alt="Favorites icon" className="navigationIcon" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/profile/profile">
+              <a>
+                <img src="/svgs/man-avatar.svg" alt="Profile icon" className="navigationIcon" />
+              </a>
+            </Link>
+          </li>
+        </ul>
       </nav>
     </>
   );
